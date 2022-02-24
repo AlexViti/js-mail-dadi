841dd775ebbab03aa8de524ceab28f7ae987bd59
AlexViti <alexviti12@gmail.com>
2022-02-24 14:33:20 +0100

    Create a list of student

--- email/js/script.js
+++ email/js/script.js
@@ -5,3 +5,13 @@ btnSend.addEventListener('click', function() {
 	let emailValue = inputEmail.value;
 	console.log(emailValue);
 });
+
+let studentList = document.getElementById('classe56').innerText.toLowerCase();
+studentList = studentList.split(';');
+console.dir(studentList);
+
+for (let i = 0; i < studentList.length; i++) {
+	studentList[i] = studentList[i].split(',');
+}
+
+console.dir(studentList);
